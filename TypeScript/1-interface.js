3c5778439062707475da6f4e40f63b18ce9847be
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2024-11-04 18:48:37 +0200

    Fix TypeScript example

--- TypeScript/1-interface.js
+++ TypeScript/1-interface.js
@@ -1,29 +1,34 @@
-class Product {
-    name;
-    productPrice;
-    constructor(name, price) {
-        this.name = name;
-        this.productPrice = price;
-    }
-    get price() {
-        return this.productPrice;
-    }
-}
+'use strict';
 
+class Product {
+  name;
+  productPrice;
+
+  constructor(name, price) {
+    this.name = name;
+    this.productPrice = price;
+  }
+
+  get price() {
+    return this.productPrice;
+  }
+}
 class Collection {
-    name;
-    collection;
-    constructor(name, ...products) {
-        this.name = name;
-        this.collection = new Set(products);
+  name;
+  collection;
+
+  constructor(name, ...products) {
+    this.name = name;
+    this.collection = new Set(products);
+  }
+
+  get price() {
+    let price = 0;
+    for (const item of this.collection) {
+      price += item.price;
     }
-    get price() {
-        let price = 0;
-        for (const item of this.collection) {
-            price += item.price;
-        }
-        return price;
-    }
+    return price;
+  }
 }
 
 // Usage
@@ -33,12 +38,9 @@ const p2 = new Product('Mouse', 25);
 const p3 = new Product('Keyboard', 100);
 const p4 = new Product('HDMI cable', 10);
 const electronics = new Collection('Electronics', p1, p2, p3, p4);
-
 const p5 = new Product('Bag', 50);
 const p6 = new Product('Mouse pad', 5);
 const textile = new Collection('Textile', p5, p6);
-
 const purchase = new Collection('Purchase', electronics, textile);
-
 console.dir(purchase, { depth: null });
 console.log(`Total is ${purchase.price}`);
